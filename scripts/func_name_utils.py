8a2f60fef2a7042f9059e7221f90083908a54223
Mizore Nekoyanagi <37854026+SleetCat123@users.noreply.github.com>
2023-03-09 01:32:54 +0900

    fix format

--- scripts/func_name_utils.py
+++ scripts/func_name_utils.py
@@ -21,26 +21,26 @@ from . import consts
 
 def add_suffix(obj):
     if consts.EXPORT_TEMP_SUFFIX not in obj.name:
-        newname = obj.name + consts.EXPORT_TEMP_SUFFIX
-        print("Add Suffix (Object name): [" + obj.name + "] -> [" + newname + "]")
-        obj.name = newname
+        new_name = obj.name + consts.EXPORT_TEMP_SUFFIX
+        print("Add Suffix (Object name): [" + obj.name + "] -> [" + new_name + "]")
+        obj.name = new_name
 
     # インスタンス化されたメッシュがあるとインスタンスの個数分だけ関数が呼ばれるため、suffixが多重追加されないように対策しておく
-    if obj.data is not None and not consts.EXPORT_TEMP_SUFFIX in obj.data.name:
-        newname = obj.data.name + consts.EXPORT_TEMP_SUFFIX
-        print("Add Suffix (Data name): [" + obj.data.name + "] -> [" + newname + "]")
-        obj.data.name = newname
+    if obj.data is not None and consts.EXPORT_TEMP_SUFFIX not in obj.data.name:
+        new_name = obj.data.name + consts.EXPORT_TEMP_SUFFIX
+        print("Add Suffix (Data name): [" + obj.data.name + "] -> [" + new_name + "]")
+        obj.data.name = new_name
 
 
 def remove_suffix(obj):
     if consts.EXPORT_TEMP_SUFFIX in obj.name:
-        oldname = obj.name
-        newname = oldname[0:oldname.rfind(consts.EXPORT_TEMP_SUFFIX)]
-        print("Remove Suffix (Object name): [" + oldname + "] -> [" + newname + "]")
-        obj.name = newname
+        old_name = obj.name
+        new_name = old_name[0:old_name.rfind(consts.EXPORT_TEMP_SUFFIX)]
+        print("Remove Suffix (Object name): [" + old_name + "] -> [" + new_name + "]")
+        obj.name = new_name
 
     if obj.data is not None and consts.EXPORT_TEMP_SUFFIX in obj.data.name:
-        oldname = obj.data.name
-        newname = oldname[0:oldname.rfind(consts.EXPORT_TEMP_SUFFIX)]
-        print("Remove Suffix (Data name): [" + oldname + "] -> [" + newname + "]")
-        obj.data.name = newname
+        old_name = obj.data.name
+        new_name = old_name[0:old_name.rfind(consts.EXPORT_TEMP_SUFFIX)]
+        print("Remove Suffix (Data name): [" + old_name + "] -> [" + new_name + "]")
+        obj.data.name = new_name
