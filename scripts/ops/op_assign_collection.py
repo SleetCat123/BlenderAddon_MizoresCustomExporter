55fd775e279f8e62b0f65ff6d75b2416a9b06632
Mizore Nekoyanagi <37854026+SleetCat123@users.noreply.github.com>
2023-07-07 17:46:11 +0900

    import修正

--- scripts/ops/op_assign_collection.py
+++ scripts/ops/op_assign_collection.py
@@ -17,7 +17,8 @@
 # ##### END GPL LICENSE BLOCK #####
 
 import bpy
-from ... import consts, func_collection_utils
+from .. import consts
+from ..funcs import func_collection_utils
 
 
 # 選択オブジェクトをDontExportグループに入れたり外したりするクラス
